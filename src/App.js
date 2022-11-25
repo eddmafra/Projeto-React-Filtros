66d19d1e612f6facb1f4821b793b3342e2f1bdc8
Eddmafra <eduardo_mafra@hotmail.com>
2022-11-25 18:06:45 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Table from './components/Table';
+import PlanetContext from './context/PlanetContext';
+import fetchPlanets from './services/fetchPlanets';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+  useEffect(() => {
+    async function planetsAPI() {
+      const data = await fetchPlanets();
+      setPlanets(data);
+    }
+    planetsAPI();
+  }, []);
+
   return (
-    <Table />
+    <PlanetContext.Provider value={ planets }>
+      <Table />
+    </PlanetContext.Provider>
   );
 }
 
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,20 @@
-import { useEffect, useState } from 'react';
-import fetchPlanets from '../services/fetchPlanets';
+import { useEffect, useState, useContext } from 'react';
+import PlanetContext from '../context/PlanetContext';
 
 export default function Table() {
-  const [planets, setPlanets] = useState([]);
-  useEffect(() => {
-    async function planetsAPI() {
-      const data = await fetchPlanets();
-      setPlanets(data);
-    }
-    planetsAPI();
-  }, []);
+  const [filtered, setFilter] = useState('');
+  const data = useContext(PlanetContext);
 
   return (
     <div>
+      <input
+        type="text"
+        data-testid="name-filter"
+        value={ filtered }
+        onChange={ ({ target }) => {
+          setFilter(target.value);
+        } }
+      />
       <table>
         <thead>
           <tr>
@@ -32,24 +34,24 @@ export default function Table() {
           </tr>
         </thead>
         <tbody>
-          {console.log(planets)}
-          {planets.map((e) => (
-            <tr key={ e.name }>
-              <td data-testid="planet-name">{e.name}</td>
-              <td>{e.rotation_period}</td>
-              <td>{e.orbital_period}</td>
-              <td>{e.diameter}</td>
-              <td>{e.climate}</td>
-              <td>{e.gravity}</td>
-              <td>{e.terrain}</td>
-              <td>{e.surface_water}</td>
-              <td>{e.population}</td>
-              <td>{e.films}</td>
-              <td>{e.created}</td>
-              <td>{e.edited}</td>
-              <td>{e.url}</td>
-            </tr>
-          ))}
+          {data.filter((el) => el.name.toLowerCase().includes(filtered.toLowerCase()))
+            .map((e) => (
+              <tr key={ e.name }>
+                <td data-testid="planet-name">{e.name}</td>
+                <td>{e.rotation_period}</td>
+                <td>{e.orbital_period}</td>
+                <td>{e.diameter}</td>
+                <td>{e.climate}</td>
+                <td>{e.gravity}</td>
+                <td>{e.terrain}</td>
+                <td>{e.surface_water}</td>
+                <td>{e.population}</td>
+                <td>{e.films}</td>
+                <td>{e.created}</td>
+                <td>{e.edited}</td>
+                <td>{e.url}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
