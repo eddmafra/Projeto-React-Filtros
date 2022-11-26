d167c353ddd1843e3416198ff071785bdf44ae3f
Eddmafra <eduardo_mafra@hotmail.com>
2022-11-26 10:24:18 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ import fetchPlanets from './services/fetchPlanets';
 
 function App() {
   const [planets, setPlanets] = useState([]);
+  const [filtered, setFilter] = useState('');
+  const [filtersCombo, setFiltersCombo] = useState([]);
+  const [filteredColumn, setFilterColumn] = useState('population');
+  const [filteredComparison, setFilterComparison] = useState('maior que');
+  const [filteredValue, setFilterValue] = useState('0');
+
+  const context = {
+    planets,
+    setPlanets,
+    filtersCombo,
+    setFiltersCombo,
+    filtered,
+    setFilter,
+    filteredColumn,
+    setFilterColumn,
+    filteredComparison,
+    setFilterComparison,
+    filteredValue,
+    setFilterValue,
+  };
+
   useEffect(() => {
     async function planetsAPI() {
       const data = await fetchPlanets();
@@ -15,7 +36,7 @@ function App() {
   }, []);
 
   return (
-    <PlanetContext.Provider value={ planets }>
+    <PlanetContext.Provider value={ context }>
       <Table />
     </PlanetContext.Provider>
   );
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,20 +1,105 @@
-import { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import PlanetContext from '../context/PlanetContext';
 
 export default function Table() {
-  const [filtered, setFilter] = useState('');
-  const data = useContext(PlanetContext);
+  const {
+    planets,
+    setPlanets,
+    filtersCombo,
+    setFiltersCombo,
+    filtered,
+    setFilter,
+    filteredColumn,
+    setFilterColumn,
+    filteredComparison,
+    setFilterComparison,
+    filteredValue,
+    setFilterValue } = useContext(PlanetContext);
+
+  const valueFilter = () => {
+    const nameFiltered = planets
+      .filter((el) => el.name.toLowerCase().includes(filtered.toLowerCase()));
+    let filterColumnPlanets = [];
+    if (filteredComparison === 'maior que') {
+      console.log(planets);
+      filterColumnPlanets = nameFiltered
+        .filter((e) => Number(e[filteredColumn]) > Number(filteredValue));
+    }
+    if (filteredComparison === 'menor que') {
+      filterColumnPlanets = nameFiltered
+        .filter((e) => Number(e[filteredColumn]) < Number(filteredValue));
+    }
+    if (filteredComparison === 'igual a') {
+      // console.log(planets);
+      filterColumnPlanets = nameFiltered
+        .filter((e) => (e[filteredColumn]) === (filteredValue));
+    }
+    // console.log(filterColumnPlanets);
+    setPlanets(filterColumnPlanets);
+  };
+
+  const getFiltered = () => {
+    // console.log(filtered, filteredColumn, filteredComparison, filteredValue);
+    // console.log(planets);
+    setFiltersCombo([...filtersCombo,
+      { filteredColumn, filteredComparison, filteredValue }]);
+    valueFilter();
+  };
 
   return (
     <div>
-      <input
-        type="text"
-        data-testid="name-filter"
-        value={ filtered }
-        onChange={ ({ target }) => {
-          setFilter(target.value);
-        } }
-      />
+      <div>
+        <input
+          type="text"
+          data-testid="name-filter"
+          value={ filtered }
+          onChange={ ({ target }) => {
+            setFilter(target.value);
+          } }
+        />
+      </div>
+      <div>
+        <select
+          name="column-filter"
+          id="column-filter"
+          data-testid="column-filter"
+          value={ filteredColumn }
+          onChange={ ({ target }) => setFilterColumn(target.value) }
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+        <select
+          name="comparison-filter"
+          id="comparison-filter"
+          data-testid="comparison-filter"
+          value={ filteredComparison }
+          onChange={ ({ target }) => setFilterComparison(target.value) }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          type="number"
+          name="value-filter"
+          id="value-filter"
+          data-testid="value-filter"
+          value={ filteredValue }
+          onChange={ ({ target }) => setFilterValue(target.value) }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ getFiltered }
+        >
+          Filtrar
+
+        </button>
+      </div>
       <table>
         <thead>
           <tr>
@@ -34,24 +119,23 @@ export default function Table() {
           </tr>
         </thead>
         <tbody>
-          {data.filter((el) => el.name.toLowerCase().includes(filtered.toLowerCase()))
-            .map((e) => (
-              <tr key={ e.name }>
-                <td data-testid="planet-name">{e.name}</td>
-                <td>{e.rotation_period}</td>
-                <td>{e.orbital_period}</td>
-                <td>{e.diameter}</td>
-                <td>{e.climate}</td>
-                <td>{e.gravity}</td>
-                <td>{e.terrain}</td>
-                <td>{e.surface_water}</td>
-                <td>{e.population}</td>
-                <td>{e.films}</td>
-                <td>{e.created}</td>
-                <td>{e.edited}</td>
-                <td>{e.url}</td>
-              </tr>
-            ))}
+          {planets.map((e) => (
+            <tr key={ e.name }>
+              <td data-testid="planet-name">{e.name}</td>
+              <td>{e.rotation_period}</td>
+              <td>{e.orbital_period}</td>
+              <td>{e.diameter}</td>
+              <td>{e.climate}</td>
+              <td>{e.gravity}</td>
+              <td>{e.terrain}</td>
+              <td>{e.surface_water}</td>
+              <td>{e.population}</td>
+              <td>{e.films}</td>
+              <td>{e.created}</td>
+              <td>{e.edited}</td>
+              <td>{e.url}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
