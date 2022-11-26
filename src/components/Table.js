8256a64e6cbe34b115f39eb310e5ede6ef6831bb
Eddmafra <eduardo_mafra@hotmail.com>
2022-11-26 10:36:34 -0300

    fix: requisito 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -119,23 +119,25 @@ export default function Table() {
           </tr>
         </thead>
         <tbody>
-          {planets.map((e) => (
-            <tr key={ e.name }>
-              <td data-testid="planet-name">{e.name}</td>
-              <td>{e.rotation_period}</td>
-              <td>{e.orbital_period}</td>
-              <td>{e.diameter}</td>
-              <td>{e.climate}</td>
-              <td>{e.gravity}</td>
-              <td>{e.terrain}</td>
-              <td>{e.surface_water}</td>
-              <td>{e.population}</td>
-              <td>{e.films}</td>
-              <td>{e.created}</td>
-              <td>{e.edited}</td>
-              <td>{e.url}</td>
-            </tr>
-          ))}
+          {planets
+            .filter((el) => el.name.toLowerCase().includes(filtered.toLowerCase()))
+            .map((e) => (
+              <tr key={ e.name }>
+                <td data-testid="planet-name">{e.name}</td>
+                <td>{e.rotation_period}</td>
+                <td>{e.orbital_period}</td>
+                <td>{e.diameter}</td>
+                <td>{e.climate}</td>
+                <td>{e.gravity}</td>
+                <td>{e.terrain}</td>
+                <td>{e.surface_water}</td>
+                <td>{e.population}</td>
+                <td>{e.films}</td>
+                <td>{e.created}</td>
+                <td>{e.edited}</td>
+                <td>{e.url}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
